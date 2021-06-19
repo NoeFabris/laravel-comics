e00132c97fcba9e0b9816c64e52a5b743dcb7adf
NoeFabris <noe@elysresidence.com>
2021-06-19 16:35:04 +0200

    Pt.1

---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,29 @@
+<header>
+{{-- Blue line --}}
+    <section class='blue_line'>
+        <P>DC POWER VISA</P>
+        <P>ADDITIONAL DC SITES</P>
+    </section>
+
+    <nav>
+        <img src="{{ asset('images/dc-logo.png') }}" alt="">
+        <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+        </ul>
+        <input type="text" placeholder='Search'>
+    
+    </nav>
+
+    <section class='jumbotron'>
+    </section>
+
+</header>
