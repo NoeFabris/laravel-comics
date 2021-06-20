886d6ab959afca12fdfaabb54171c9a039eeb05f
NoeFabris <noe@elysresidence.com>
2021-06-20 02:23:01 +0200

    Pt. 3

--- laravel-comics/resources/views/partials/footer.blade.php
+++ laravel-comics/resources/views/partials/footer.blade.php
@@ -1,6 +1,71 @@
-<footer>
+<footer class='bg-grey'>
 
+    <section class='bg-footer-img'>
 
+        <div class="container">
+            <div>
+
+                <h3>DC COMICS</h3>
+                <ul>
+
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+
+                </ul>
+                <h3>SHOP</h3>
+                <ul>
+                
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+
+                </ul>
+
+            </div>
+            <div>
+
+                <h3>DC</h3>
+                <ul>
+
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+
+                </ul>
+
+            </div>
+            <div>
+
+                <h3>SITES</h3>
+                <ul>
+
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Characters</a></li>
+
+                </ul>
+
+            </div>
+        </div>
+        <div class="container">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas blanditiis illum suscipit iusto incidunt vel alias saepe.</p>
+        </div>
+
+    </section>
 
 
 
--- laravel-comics/resources/views/partials/main.blade.php
+++ laravel-comics/resources/views/partials/main.blade.php
@@ -1,7 +1,55 @@
 <main>
 
-    <section class=''>
-    
+    <section class='bg-grey'>
+
+        <div class='container'>
+
+            <div class='card'>
+
+            </div>
+            <div class='load-more-btn'>
+                <button>LOAD MORE</button>
+            </div>
+
+        </div>
+
+    </section>
+    <section class='bg-blue'>
+
+        <div class='container blue-menu'>
+
+            <div>
+
+                <img src="{{ asset('images/buy-comics-digital-comics.png')}}" alt="">
+                <p>DIGITAL COMICS</p>
+
+            </div>
+            <div>
+
+                <img src="{{ asset('images/buy-comics-merchandise.png')}}" alt="">
+                <p>DC MERCHANDISE</p>
+
+            </div>
+            <div>
+
+                <img src="{{ asset('images/buy-comics-subscriptions.png')}}" alt="">
+                <p>SUBSCRIPTIONS</p>
+
+            </div>
+            <div>
+
+                <img src="{{ asset('images/buy-comics-shop-locator.png')}}" alt="">
+                <p>COMIC SHOP LOCATOR</p>
+
+            </div>
+            <div>
+
+                <img src="{{ asset('images/buy-dc-power-visa.svg')}}" alt="">
+                <p>DC POWER VISA</p>
+
+            </div>
+            
+        </div>
     </section>
 
 </main>
